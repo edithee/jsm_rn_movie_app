9995681f22621457fdc30ae2371511ebba459ea9
edithee <editheefoodie@gmail.com>
2025-04-17 18:34:18 +0800

    add routing

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from "react-native";
+
+const Onboarding = () =>{
+    return (
+        <View>
+            <Text>all aboaaardd</Text>
+        </View>
+
+    );
+}
+
+export default Onboarding;
+const styles = StyleSheet.create({});
